946b619273fae8228329f64cb83a1ecfdde5f984
Manan Chopra <manan.gchopra@gmail.com>
2023-07-01 13:44:32 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require('sequelize');
 
 // Create a new Sequelize instance
+// const sequelize = new Sequelize({
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   username: 'Manan_chopra',
+//   password: 'P@s$w0rd123!',
+//   database: 'FluxKart',
+// });
 const sequelize = new Sequelize({
   dialect: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: 'Manan_chopra',
-  password: 'P@s$w0rd123!',
-  database: 'FluxKart',
+  host: "dpg-ciftu55gkuvojje5954g-a",
+  port: 5432,
+  username: "mananchopradev",
+  password: "HWdcyESwGCk6fIE7fdVtyZjXYeZb17F7",
+  database: "devfluxkart_7bu4"
 });
 
 sequelize
