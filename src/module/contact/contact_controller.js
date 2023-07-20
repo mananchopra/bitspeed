6cc92b9f2deb88ee1f51f1fbf079cb78b417b1d2
Manan Chopra <manan.gchopra@gmail.com>
2023-07-20 14:39:15 +0530

    changed controller params from query to body

--- src/module/contact/contact_controller.js
+++ src/module/contact/contact_controller.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 const contactService = require("./contact_service")
 
 router.post('/identify', async(req, res, next) => {
-    const result = await contactService.getContacts(req.query.email, req.query.phoneNumber)
-    return res.json(result);
-
+    if(!req.body.email && !req.body.phoneNumber) {
+        return res.status(400).json('both email and phoneNumber are null')
+    }
+    const result = await contactService.getContacts(req.body.email, req.body.phoneNumber)
+    return res.status(200).json(result);
 })
 
 router.post('/createContact', async(req, res, next) => {
     console.log(JSON.stringify(req.body))
     const result = await contactService.createContact(req.body)
-    return res.json(result);
-
+    return res.status(200).json(result);
 })
 
 module.exports = router
